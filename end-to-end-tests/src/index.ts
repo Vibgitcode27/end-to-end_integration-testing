36a309a40074c723b77ff1ca7cd98b4e12bcd574
vibhor <vibgitcode27@gmail.com>
2024-06-20 18:53:52 +0530

    ci/cd test commit

--- end-to-end-tests/src/index.ts
+++ end-to-end-tests/src/index.ts
@@ -27,3 +27,26 @@ app.post("/sum", async (req, res) => {
 
   res.json({ answer: result, id: request.id });
 });
+
+app.post("/dummy", async (req, res) => {
+  const a = req.body.a;
+  const b = req.body.b;
+
+  if (a > 1000000 || b > 1000000) {
+    return res.status(422).json({
+      message: "Sorry we dont support big numbers",
+    });
+  }
+  const result = a + b;
+
+  const request = await prismaClient.request.create({
+    data: {
+      a: a,
+      b: b,
+      answer: result,
+      type: "ADD",
+    },
+  });
+
+  res.json({ answer: result, id: request.id });
+});
